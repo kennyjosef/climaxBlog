6190e5c04350c188d5055e576cee9246499d7933
kennyjosef <idowukehinde28@gmail.com>
2022-09-15 21:52:20 +0100

    worked on the miniblog overflow

--- js.js
+++ js.js
@@ -79,6 +79,44 @@ function myFunctionText() {
   const dotsTwo= document.getElementById('dots-two')
   const btnTwo = document.getElementById('btn-two')
   const moreTwo = document.getElementById('more-two')
+  const dotsThree= document.getElementById('dots-three')
+  const btnThree = document.getElementById('btn-three')
+  const moreThree = document.getElementById('more-three')
+  const dotsFour= document.getElementById('dots-four')
+  const btnFour = document.getElementById('btn-four')
+  const moreFour = document.getElementById('more-four')
+  const dotsFive= document.getElementById('dots-five')
+  const btnFive = document.getElementById('btn-five')
+  const moreFive = document.getElementById('more-five')
+  const dotsSix= document.getElementById('dots-six')
+  const btnSix = document.getElementById('btn-six')
+  const moreSix = document.getElementById('more-six')
+  const dotsSeven= document.getElementById('dots-seven')
+  const btnSeven = document.getElementById('btn-seven')
+  const moreSeven = document.getElementById('more-seven')
+
+  // function myReadMoreAndLess(){
+  //   if(dotsOne.style.display==='none'){
+  //     dotsOne.style.display='inline';
+  //     btnOne.innerHTML='Read more';
+  //     moreOne.style.display='none';
+  //   }else{
+  //     dotsOne.style.display='none';
+  //     btnOne.innerHTML='Read less';
+  //     moreOne.style.display='inline';
+  //   }
+  // }
+  function myBlogSeven (){
+    if(dotsSeven.style.display==='none'){
+      dotsSeven.style.display='inline';
+      btnSeven.innerHTML='Read more';
+      moreSeven.style.display='none';
+    }else{
+      dotsSeven.style.display='none';
+      btnSeven.innerHTML='Read less';
+      moreSeven.style.display='inline';
+    }
+  }
   function myBlogOne (){
     if(dotsOne.style.display==='none'){
       dotsOne.style.display='inline';
@@ -102,8 +140,57 @@ function myFunctionText() {
       moreTwo.style.display='inline';
     }
   }
+  function myBlogThree (){
+    if(dotsThree.style.display==='none'){
+      dotsThree.style.display='inline';
+      btnThree.innerHTML='Read more';
+      moreThree.style.display='none';
+    }else{
+      dotsThree.style.display='none';
+      btnThree.innerHTML='Read less';
+      moreThree.style.display='inline';
+    }
+  }
+  function myBlogFour (){
+    if(dotsFour.style.display==='none'){
+      dotsFour.style.display='inline';
+      btnFour.innerHTML='Read more';
+      moreFour.style.display='none';
+    }else{
+      dotsFour.style.display='none';
+      btnFour.innerHTML='Read less';
+      moreFour.style.display='inline';
+    }
+  }
+  function myBlogFive (){
+    if(dotsFive.style.display==='none'){
+      dotsFive.style.display='inline';
+      btnFive.innerHTML='Read more';
+      moreFive.style.display='none';
+    }else{
+      dotsFive.style.display='none';
+      btnFive.innerHTML='Read less';
+      moreFive.style.display='inline';
+    }
+  }
+  function myBlogSix (){
+    if(dotsSix.style.display==='none'){
+      dotsSix.style.display='inline';
+      btnSix.innerHTML='Read more';
+      moreSix.style.display='none';
+    }else{
+      dotsSix.style.display='none';
+      btnSix.innerHTML='Read less';
+      moreSix.style.display='inline';
+    }
+  }
   btnOne.addEventListener('click', myBlogOne)
   btnTwo.addEventListener('click', myBlogTwo)
+  btnThree.addEventListener('click', myBlogThree)
+  btnFour.addEventListener('click', myBlogFour)
+  btnFive.addEventListener('click', myBlogFive)
+  btnSix.addEventListener('click', myBlogSix)
+  btnSeven.addEventListener('click', myBlogSeven)
 
   //   btnText.addEventListener('click', ()=>{
 //     moreText.style.display="block"
